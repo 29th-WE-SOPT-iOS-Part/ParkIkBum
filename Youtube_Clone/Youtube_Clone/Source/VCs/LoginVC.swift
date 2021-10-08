3b300268fdbdeb34340a5abf817193128e0c46e1
parkikbum <pibum@naver.com>
2021-10-08 14:18:11 +0900

    ➕[ADD] 프로젝트 초기 설정

---
+++ Youtube_Clone/Youtube_Clone/Source/VCs/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  Youtube_Clone
+//
+//  Created by 박익범 on 2021/10/08.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ Youtube_Clone/Youtube_Clone/Source/VCs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube_Clone
+//
+//  Created by 박익범 on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
