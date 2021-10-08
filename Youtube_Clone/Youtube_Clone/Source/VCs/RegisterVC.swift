05cd5fbb0a52d6e68248181e0804c63de4b2f8a3
parkikbum <pibum@naver.com>
2021-10-08 21:12:36 +0900

    ✨[FEAT]#1-도전과제 2번 구현 및 회원가입 뷰 완성

--- Youtube_Clone/Youtube_Clone/Source/VCs/RegisterVC.swift
+++ Youtube_Clone/Youtube_Clone/Source/VCs/RegisterVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVC.swift
+//  RegisterVC.swift
 //  Youtube_Clone
 //
 //  Created by 박익범 on 2021/10/08.
@@ -7,23 +7,19 @@
 
 import UIKit
 import SnapKit
-import Then
 
-class LoginVC: UIViewController {
-//MARK: VAR
+class RegisterVC: UIViewController {
+    
+    var checkCnt: Int = 0;
+    
+    
     private let logoImageView = UIImageView().then{
         $0.image = UIImage(named: "google")
     }
     private let LoginHeadLabel = UILabel().then{
-        $0.text = "로그인"
+        $0.text = "회원가입"
         $0.font = UIFont.boldSystemFont(ofSize: 40)
         $0.textAlignment = .center
-    }
-    private let explaneLabel = UILabel().then{
-        $0.textColor = UIColor.lightGray
-        $0.text = "Youtube도 이동하며 계속하세요. 앱 및 Safari에서도 Google 서비스에 로그인 됩니다."
-        $0.textAlignment = .center
-        $0.numberOfLines = 2
     }
     private let nameTextField = UITextField().then{
         $0.layer.borderWidth = 1
@@ -45,10 +41,7 @@ class LoginVC: UIViewController {
         $0.layer.cornerRadius = 10
         $0.clipsToBounds = true
         $0.placeholder = "  비밀번호 입력"
-    }
-    private let makeAccoutButton = UIButton().then{
-        $0.setTitleColor(UIColor.systemBlue, for: .normal)
-        $0.setTitle("계정만들기", for: .normal)
+        $0.isSecureTextEntry = true
     }
     private let nextButton = UIButton().then{
         $0.setTitle("다음", for: .normal)
@@ -57,57 +50,58 @@ class LoginVC: UIViewController {
         $0.clipsToBounds = true
         $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         $0.backgroundColor = UIColor(red: 66.0/255.0, green: 134.0/255.0, blue: 244.0/255.0, alpha: 1.0)
-        $0.addTarget(self, action: #selector(nextButtonClicked(_:)), for: .touchUpInside)
+    }
+    private let checkButton = UIButton().then{
+        $0.setBackgroundImage(UIImage(named: "unchecked"), for: .normal)
+        $0.addTarget(self, action: #selector(isCheckedClicked(_:)), for: .touchUpInside)
 
     }
-    
-//MARK: ViewdidLoad
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setLayout()
-        self.view.backgroundColor = UIColor.white
+    private let checkLabel = UILabel().then{
+        $0.text = "비밀번호 표시"
+        $0.font = UIFont.systemFont(ofSize: 15)
     }
 
-//MARK: Function
-    @objc private func nextButtonClicked(_ sender: UIButton){
-        guard let registerVC = UIStoryboard(name: "Register", bundle: nil).instantiateViewController(withIdentifier: "RegisterVC") as? RegisterVC else {return}
+//MARK: Func
+    @objc private func isCheckedClicked(_ sender: UIButton){
+        if checkCnt == 0{
+        checkButton.setBackgroundImage(UIImage(named: "checked"), for: .normal)
+        pwdTextField.isSecureTextEntry = false
+        checkCnt = 1
+        }
+        else if checkCnt == 1{
+            checkButton.setBackgroundImage(UIImage(named: "unchecked"), for: .normal)
+            pwdTextField.isSecureTextEntry = true
+            checkCnt = 0
+        }
         
-        self.present(registerVC, animated: true, completion: nil)
+        
     }
-
-    
     
     func setLayout(){
         self.view.addSubview(logoImageView)
         self.view.addSubview(LoginHeadLabel)
-        self.view.addSubview(explaneLabel)
         self.view.addSubview(nameTextField)
         self.view.addSubview(emailTextField)
         self.view.addSubview(pwdTextField)
-        self.view.addSubview(makeAccoutButton)
+        self.view.addSubview(checkButton)
         self.view.addSubview(nextButton)
+        self.view.addSubview(checkLabel)
 
         //top left bottom right
         logoImageView.snp.makeConstraints{
             $0.top.equalToSuperview().offset(60)
-            $0.left.equalToSuperview().offset(140)
-            $0.right.equalToSuperview().offset(-140)
+            $0.leading.equalToSuperview().offset(140)
+            $0.trailing.equalToSuperview().offset(-140)
             $0.height.equalTo(40)
         }
         LoginHeadLabel.snp.makeConstraints{
             $0.top.equalTo(logoImageView.snp.bottom).offset(25)
-            $0.left.equalToSuperview().offset(50)
-            $0.right.equalToSuperview().offset(-50)
+            $0.leading.equalToSuperview().offset(50)
+            $0.trailing.equalToSuperview().offset(-50)
             $0.height.equalTo(50)
         }
-        explaneLabel.snp.makeConstraints{
-            $0.top.equalTo(LoginHeadLabel.snp.bottom).offset(30)
-            $0.left.equalToSuperview().offset(20)
-            $0.right.equalToSuperview().offset(-20)
-            $0.height.equalTo(60)
-        }
         nameTextField.snp.makeConstraints{
-            $0.top.equalTo(explaneLabel.snp.bottom).offset(30)
+            $0.top.equalTo(LoginHeadLabel.snp.bottom).offset(90)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(350)
             $0.height.equalTo(60)
@@ -124,19 +118,31 @@ class LoginVC: UIViewController {
             $0.width.equalTo(350)
             $0.height.equalTo(60)
         }
-        makeAccoutButton.snp.makeConstraints{
-            $0.top.equalTo(pwdTextField.snp.bottom).offset(50)
-            $0.left.equalToSuperview().offset(20)
-            $0.height.equalTo(50)
-            $0.width.equalTo(80)
+        checkButton.snp.makeConstraints{
+            $0.top.equalTo(pwdTextField.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(20)
+            $0.height.equalTo(20)
+            $0.width.equalTo(20)
+        }
+        checkLabel.snp.makeConstraints{
+            $0.top.equalTo(pwdTextField.snp.bottom).offset(10)
+            $0.leading.equalTo(checkButton.snp.trailing).offset(10)
+            $0.width.equalTo(100)
         }
         nextButton.snp.makeConstraints{
-            $0.centerY.equalTo(makeAccoutButton.snp.centerY)
-            $0.right.equalToSuperview().offset(-20)
+            $0.top.equalTo(checkButton.snp.bottom).offset(60)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
             $0.height.equalTo(50)
-            $0.width.equalTo(80)
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+
+    }
+    
+
 
 }
