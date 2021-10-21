144ac115429a53ff1844515236409867d574ac00
parkikbum <pibum@naver.com>
2021-10-21 15:08:52 +0900

    ✅[CHORE]: #3 - 로그인 뷰 오토레이아웃 적용

--- Youtube_Clone/Youtube_Clone/Source/VCs/LoginVC.swift
+++ Youtube_Clone/Youtube_Clone/Source/VCs/LoginVC.swift
@@ -12,23 +12,25 @@ import Then
 class LoginVC: UIViewController {
 //MARK: VAR
     private let logoImageView = UIImageView().then{
-        $0.image = UIImage(named: "google")
+        $0.image = UIImage(named: "logo")
     }
     private let LoginHeadLabel = UILabel().then{
         $0.text = "로그인"
-        $0.font = UIFont.boldSystemFont(ofSize: 40)
+        $0.font = UIFont.boldSystemFont(ofSize: 22)
         $0.textAlignment = .center
     }
     private let explaneLabel = UILabel().then{
-        $0.textColor = UIColor.lightGray
-        $0.text = "Youtube도 이동하며 계속하세요. 앱 및 Safari에서도 Google 서비스에 로그인 됩니다."
+        $0.textColor = UIColor.black
+        $0.text = "Youtube도 이동하며 계속하세요. \n앱 및 Safari에서도 Google 서비스에 로그인 됩니다."
+        $0.font = UIFont.systemFont(ofSize: 14)
         $0.textAlignment = .center
         $0.numberOfLines = 2
     }
     private let nameTextField = UITextField().then{
         $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.lightGray.cgColor
+        $0.layer.borderColor = UIColor.systemGray6.cgColor
         $0.layer.cornerRadius = 10
+        $0.font = UIFont.systemFont(ofSize: 15)
         $0.clipsToBounds = true
         $0.placeholder = "  이름을 입력해주세요"
         $0.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
@@ -36,34 +38,37 @@ class LoginVC: UIViewController {
     }
     private let emailTextField = UITextField().then{
         $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.lightGray.cgColor
+        $0.layer.borderColor = UIColor.systemGray6.cgColor
         $0.layer.cornerRadius = 10
+        $0.font = UIFont.systemFont(ofSize: 15)
         $0.clipsToBounds = true
-        $0.placeholder = "  이메일 또는 휴대전화"
+        $0.placeholder = " 이메일 또는 휴대전화"
         $0.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
 
     }
     private let pwdTextField = UITextField().then{
         $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.lightGray.cgColor
+        $0.layer.borderColor = UIColor.systemGray6.cgColor
         $0.layer.cornerRadius = 10
+        $0.font = UIFont.systemFont(ofSize: 15)
         $0.clipsToBounds = true
-        $0.placeholder = "  비밀번호 입력"
+        $0.placeholder = " 비밀번호 입력"
         $0.isSecureTextEntry = true
         $0.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
     }
     private let makeAccoutButton = UIButton().then{
         $0.setTitleColor(UIColor.systemBlue, for: .normal)
         $0.setTitle("계정만들기", for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14)
         $0.addTarget(self, action: #selector(registerButtonClicked(_:)), for: .touchUpInside)
 
     }
     private let nextButton = UIButton().then{
         $0.setTitle("다음", for: .normal)
         $0.setTitleColor(UIColor.white, for: .normal)
-        $0.layer.cornerRadius = 10
+        $0.layer.cornerRadius = 5
         $0.clipsToBounds = true
-        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         $0.backgroundColor = UIColor.systemGray
         $0.addTarget(self, action: #selector(nextButtonClicked(_:)), for: .touchUpInside)
 
@@ -123,52 +128,52 @@ class LoginVC: UIViewController {
 
         //top left bottom right
         logoImageView.snp.makeConstraints{
-            $0.top.equalToSuperview().offset(60)
-            $0.left.equalToSuperview().offset(140)
-            $0.right.equalToSuperview().offset(-140)
+            $0.top.equalToSuperview().offset(110)
+            $0.left.equalToSuperview().offset(130)
+            $0.right.equalToSuperview().offset(-130)
             $0.height.equalTo(40)
         }
         LoginHeadLabel.snp.makeConstraints{
-            $0.top.equalTo(logoImageView.snp.bottom).offset(25)
-            $0.left.equalToSuperview().offset(50)
-            $0.right.equalToSuperview().offset(-50)
-            $0.height.equalTo(50)
+            $0.top.equalTo(logoImageView.snp.bottom).offset(23)
+            $0.left.equalToSuperview().offset(157)
+            $0.right.equalToSuperview().offset(-157)
+            $0.height.equalTo(24)
         }
         explaneLabel.snp.makeConstraints{
-            $0.top.equalTo(LoginHeadLabel.snp.bottom).offset(30)
-            $0.left.equalToSuperview().offset(20)
-            $0.right.equalToSuperview().offset(-20)
+            $0.top.equalTo(LoginHeadLabel.snp.bottom).offset(14)
+            $0.left.equalToSuperview().offset(44)
+            $0.right.equalToSuperview().offset(-44)
             $0.height.equalTo(60)
         }
         nameTextField.snp.makeConstraints{
-            $0.top.equalTo(explaneLabel.snp.bottom).offset(30)
+            $0.top.equalTo(explaneLabel.snp.bottom).offset(68)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(350)
-            $0.height.equalTo(60)
+            $0.width.equalTo(331)
+            $0.height.equalTo(48)
         }
         emailTextField.snp.makeConstraints{
             $0.top.equalTo(nameTextField.snp.bottom).offset(10)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(350)
-            $0.height.equalTo(60)
+            $0.width.equalTo(331)
+            $0.height.equalTo(48)
         }
         pwdTextField.snp.makeConstraints{
             $0.top.equalTo(emailTextField.snp.bottom).offset(10)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(350)
-            $0.height.equalTo(60)
+            $0.width.equalTo(331)
+            $0.height.equalTo(48)
         }
         makeAccoutButton.snp.makeConstraints{
-            $0.top.equalTo(pwdTextField.snp.bottom).offset(50)
-            $0.left.equalToSuperview().offset(20)
-            $0.height.equalTo(50)
-            $0.width.equalTo(80)
+            $0.top.equalTo(pwdTextField.snp.bottom).offset(73)
+            $0.left.equalToSuperview().offset(22)
+            $0.height.equalTo(22)
+            $0.width.equalTo(68)
         }
         nextButton.snp.makeConstraints{
             $0.centerY.equalTo(makeAccoutButton.snp.centerY)
-            $0.right.equalToSuperview().offset(-20)
-            $0.height.equalTo(50)
-            $0.width.equalTo(80)
+            $0.right.equalToSuperview().offset(-22)
+            $0.height.equalTo(42)
+            $0.width.equalTo(74)
         }
     }
     
