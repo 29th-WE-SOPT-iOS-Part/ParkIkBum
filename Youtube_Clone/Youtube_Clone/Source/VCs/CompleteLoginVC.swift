2ee3ce6203592ff71ff4611866756770cdaa765e
parkikbum <pibum@naver.com>
2021-10-21 15:25:37 +0900

    ✅[CHORE]:#3 - 회원가입 완료 뷰 오토레이아웃 수정완료

--- Youtube_Clone/Youtube_Clone/Source/VCs/CompleteLoginVC.swift
+++ Youtube_Clone/Youtube_Clone/Source/VCs/CompleteLoginVC.swift
@@ -15,15 +15,15 @@ class CompleteLoginVC: UIViewController {
     
     
     private let logoImageView = UIImageView().then{
-        $0.image = UIImage(named: "google")
+        $0.image = UIImage(named: "logo")
     }
     
-    private lazy var okButton = UIButton().then{
+    private var okButton = UIButton().then{
         $0.setTitle("확인", for: .normal)
         $0.setTitleColor(UIColor.white, for: .normal)
-        $0.layer.cornerRadius = 10
+        $0.layer.cornerRadius = 5
         $0.clipsToBounds = true
-        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         $0.backgroundColor = UIColor(red: 66.0/255.0, green: 134.0/255.0, blue: 244.0/255.0, alpha: 1.0)
         $0.addTarget(self, action: #selector(okButtonClicked(_:)), for: .touchUpInside)
     }
@@ -41,7 +41,7 @@ class CompleteLoginVC: UIViewController {
     func setLayout(){
         let hiLabel = UILabel().then{
             $0.text = "\(name)님 \n환영합니다."
-            $0.font = UIFont.boldSystemFont(ofSize: 40)
+            $0.font = UIFont.boldSystemFont(ofSize: 26)
             $0.numberOfLines = 2
             $0.lineBreakMode = .byCharWrapping
             $0.textAlignment = .center
@@ -53,22 +53,22 @@ class CompleteLoginVC: UIViewController {
         
         
         logoImageView.snp.makeConstraints{
-            $0.top.equalToSuperview().offset(200)
-            $0.leading.equalToSuperview().offset(140)
-            $0.trailing.equalToSuperview().offset(-140)
+            $0.top.equalToSuperview().offset(248)
+            $0.leading.equalToSuperview().offset(128)
+            $0.trailing.equalToSuperview().offset(-128)
             $0.height.equalTo(40)
         }
         hiLabel.snp.makeConstraints{
-            $0.top.equalTo(logoImageView.snp.bottom).offset(25)
-            $0.leading.equalToSuperview().offset(50)
-            $0.trailing.equalToSuperview().offset(-50)
-            $0.height.equalTo(200)
+            $0.top.equalTo(logoImageView.snp.bottom).offset(23)
+            $0.leading.equalToSuperview().offset(123)
+            $0.trailing.equalToSuperview().offset(-123)
+            $0.height.equalTo(78)
         }
         okButton.snp.makeConstraints{
-            $0.top.equalTo(hiLabel.snp.bottom).offset(30)
-            $0.leading.equalToSuperview().offset(15)
-            $0.trailing.equalToSuperview().offset(-15)
-            $0.height.equalTo(50)
+            $0.top.equalTo(hiLabel.snp.bottom).offset(53)
+            $0.leading.equalToSuperview().offset(22)
+            $0.trailing.equalToSuperview().offset(-22)
+            $0.height.equalTo(42)
         }
         
     }
