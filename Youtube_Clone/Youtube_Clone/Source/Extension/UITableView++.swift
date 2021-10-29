037b7e809a250de810d0f656fe2bff47c0a9c05c
parkikbum <pibum@naver.com>
2021-10-29 21:41:28 +0900

    ➕[ADD]:#5-테이블뷰 익스텐션

--- Youtube_Clone/Youtube_Clone/Source/Extension/UITableView++.swift
+++ Youtube_Clone/Youtube_Clone/Source/Extension/UITableView++.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension UITableView{
+    public func registerCustomXib(xibName: String){
+        let xib = UINib(nibName: xibName, bundle: nil)
+        self.register(xib, forCellReuseIdentifier: xibName)
+    }
+    
+    
+}
+
